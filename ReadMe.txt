f523677b69fa47f1ec914d098c11766b9336e1e8
Dave778 <davef778@gmail.com>
2022-01-28 18:37:18 -0600

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+Just trying to learn some CSS and JS
+
+This is my first attempt at a rock scissors paper game following the tutorial at https://youtu.be/jaVNP3nIAv0
+
+Thisis also my first project using Git. 
+
+Known issues. 
+
+I am unable to seperate all three of the images for rock, scissors, and paper. When one clicks on the paper icon, there are three outcomes at once. 
---
+++ app.js
@@ -0,0 +1,98 @@
+let userScore =0;
+let computerScore = 0;
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_p = document.querySelector('.result > p');
+const rock_div = document.getElementById("r");
+const scissors_div = document.getElementById('s');
+const paper_div = document.getElementById('p');
+
+function getComputerChoice(){
+    const choices = ['r','p','s']
+    const randomNumber = (Math.floor(Math.random()*3));
+    return choices[randomNumber];
+}
+
+function convertToWord(letter){
+    if( letter ==="r") return 'Rock';
+    if (letter ==='s') return 'Scissors';
+    return "Paper";
+}
+
+
+
+
+
+
+
+
+function win (userChoice,computerChoice){
+    console.log('WIN');
+    userScore ++;
+    userScore_span.innerHTML = userScore
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord (computerChoice)}  you win!`;
+
+}
+
+function lose (){
+    console.log('LOST');
+    computerScore ++
+    computerScore_span.innerHTML = computerScore
+}
+
+function draw (){
+    console.log('its a draawwww');
+}
+getComputerChoice();
+
+
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice){
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            
+            win();
+            break;
+        case 'rp':
+        case 'ps':
+        case 'sr':
+            
+            lose ();
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            
+            draw();
+            break;
+
+    }
+    
+}
+
+function main (){
+
+    rock_div.addEventListener('click',function(){
+        game('r');
+
+    })
+
+    scissors_div.addEventListener('click',function(){
+        game('s');
+    })
+
+    paper_div.addEventListener('click',function(){
+        game('p');
+ 
+ 
+    })
+
+
+
+}
+ main()
+
